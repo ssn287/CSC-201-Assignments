eb6fd7f7774243a1acbff6b4cfb7bfad66c2aeaf
ssn287 <shelbysneal@gmail.com>
2019-11-17 19:27:55 -0500

    boom

--- src/Cramers.java
+++ src/Cramers.java
@@ -1,6 +1,6 @@
 /**
  * Cramers class solves 3x3 systems of equations using Cramer's rule
- * @author sneal
+ * @author Shelby Neal
  * Emplid: 6030859
  * Email: ssn287@email.vccs.edu
  * Purpose: Programming Assignment #9
